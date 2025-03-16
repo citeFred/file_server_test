ad4be3b7b02babeb3dea12bbb669a81e4f51906b
citefred <citefred@gmail.com>
2025-03-16 19:07:38 +0900

    refactor: Replace Upload Physics Server into center nas

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,33 +1,47 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { mkdirSync, writeFileSync, readdirSync, readFileSync } from 'fs';
-import { join } from 'path';
+import { mkdirSync, writeFileSync, readdirSync } from 'fs';
+import { join, extname } from 'path';
 
 @Injectable()
 export class FileService {
     private readonly logger = new Logger(FileService.name);
-
-      private readonly uploadPath = '/var/services/video'; // Use Absolute Path
-    // private readonly uploadPath = join(__dirname, '../../../../../video'); // Use Relative Path
+    private readonly basePath = '/var/services/static'; // 기본 경로
 
     constructor() {
-        mkdirSync(this.uploadPath, { recursive: true });
+        mkdirSync(join(this.basePath, 'videos'), { recursive: true });
+        mkdirSync(join(this.basePath, 'images'), { recursive: true });
+        mkdirSync(join(this.basePath, 'docs'), { recursive: true });
     }
 
     saveFile(file: Express.Multer.File) {
-        this.logger.verbose(`FileService Layer trying to save file into: ${this.uploadPath}`);
+        this.logger.verbose(`FileService Layer trying to save file: ${file.originalname}`);
 
-        const filepath = join(this.uploadPath, file.originalname);
+        // 파일 확장자 확인
+        const fileExtension = extname(file.originalname).toLowerCase();
+        let uploadPath;
+
+        // 확장자에 따라 경로 설정
+        if (['.mp4', '.avi', '.mov', '.mkv'].includes(fileExtension)) {
+            uploadPath = join(this.basePath, 'videos');
+        } else if (['.jpg', '.jpeg', '.png', '.gif', '.bmp'].includes(fileExtension)) {
+            uploadPath = join(this.basePath, 'images');
+        } else {
+            uploadPath = join(this.basePath, 'docs');
+        }
+
+        const filepath = join(uploadPath, file.originalname);
         writeFileSync(filepath, file.buffer);
 
-        this.logger.verbose(`FileService had saved ${filepath}`);
+        this.logger.verbose(`FileService saved file to: ${filepath}`);
     }
 
-    getFiles() {
-        this.logger.verbose(`FileService Layer trying to get files list from: ${this.uploadPath}`);
-        return readdirSync(this.uploadPath);
+    getFiles(type: 'videos' | 'images' | 'docs') {
+        const targetPath = join(this.basePath, type);
+        this.logger.verbose(`FileService Layer trying to get files list from: ${targetPath}`);
+        return readdirSync(targetPath);
     }
 
-    getFilePath(filename: string): string {
-        return join(this.uploadPath, filename); // 파일 경로를 문자열로 반환
+    getFilePath(filename: string, type: 'videos' | 'images' | 'docs'): string {
+        return join(this.basePath, type, filename);
     }
 }
